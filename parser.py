28802ae3a2c53e4f00fa8ed25f190aa8265a47ce
Misha <epifanov0303@mail.ru>
2018-09-05 17:16:03 +0300

    parsing: title, data, href

--- parser.py
+++ parser.py
@@ -1,4 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+
 response_obj = requests.get("https://www.youtube.com/feed/trending")
+
 if response_obj.status_code == 200:
-    print(response_obj.text)
+    soup = BeautifulSoup(response_obj.text, 'html.parser')
+    elements = soup.find_all("div", "yt-lockup-content")
+    for i in elements:
+        title = i.contents[0].contents[0].text
+        hrefd = i.contents[0].contents[0]["href"]
+        time = i.contents[2].contents[0].contents[0].text
+        print("Название: " + title + "   Ссылка: " + hrefd + "    Дата: " + time)
+        
